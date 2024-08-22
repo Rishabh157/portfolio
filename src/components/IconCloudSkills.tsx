3af86a6ae7621914e1b207cb22b94b9c1f7ba7f6
Rishabh157 <rishabh.gour@codiotic.com>
2024-08-22 16:15:11 +0530

    changes in icon cloud

--- src/components/IconCloudSkills.tsx
+++ src/components/IconCloudSkills.tsx
@@ -49,7 +49,7 @@ export function IconCloudSkills() {
     return (
         <div className="bg-transparent border-[1px] border-b-[#e7e7e7] flex h-full w-full max-w-[40rem] items-center justify-center overflow-hidden rounded-lg
         desktop:px-20 laptop:px-20 tablet:px-20 mobile:px-2 desktop:pb-20 laptop:pb-20 tablet:pb-20 mobile:pb-12 desktop:pt-8 laptop:pt-8 tablet:pt-8 mobile:pt-4
-        ">
+        "> 
             <IconCloud iconSlugs={slugs} />
         </div>
     );
