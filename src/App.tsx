e4eb840dfeb5703bd0091a2e6635bcfc95fb8db9
Rishabh157 <rishabhgour157@gmail.com>
2024-11-03 19:00:02 +0530

    remvoe the commit code

--- src/App.tsx
+++ src/App.tsx
@@ -130,23 +130,6 @@ function App() {
         </div>
       </section>
 
-
-      <div className="flex flex-row items-center justify-center mb-10 w-full">
-        {/* <AnimatedTooltip items={
-          [
-            {
-              id: 1,
-              name: "Saptel",
-              designation: "Software EngineerSoftware EngineerSoftware EngineerSoftware EngineerSoftware EngineerSoftware Engineer",
-              image:
-                "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-            },
-          ]
-        } /> */}
-      </div>
-
-
-
       <div className="border-b-[1px] border-b-[#e7e7e7] mt-32"></div>
       {/* <!-- Footer --> */}
       <Footer />
